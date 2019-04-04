521ed3ab900826be3f246afde15d524fd8debcea
unknown <alessandro3.passoni@mail.polimi.it>
2019-04-04 20:47:08 +0200

    aggiunta documentazione ammopool

--- src/main/java/AmmoPool.java
+++ src/main/java/AmmoPool.java
@@ -6,25 +6,43 @@ public class AmmoPool {
 
     public static final int MAX_AMMO = 3;
 
-    private Ammo redAmmos;
-    private Ammo yellowAmmos;
-    private Ammo blueAmmos;
+    private List<Ammo> redAmmos;
+    private List<Ammo> yellowAmmos;
+    private List<Ammo> blueAmmos;
 
-    public List<Ammo> getNumBlueAmmos(int i){
-
-        return null;
-    }
-
-    public List<Ammo> getNumYellowAmmos(int i){
-
-        return null;
-    }
-
-    public List<Ammo> getNumRedAmmos(int i){
+    /**
+     * this method gives to player, if possible, blue ammos as requested
+     * @param ammoNum number of blue ammos requested
+     * @return list of blue ammos requested
+     */
+    public List<Ammo> getNumBlueAmmos(int ammoNum){
 
         return null;
     }
 
+    /**
+     * this method gives to player, if possible, yellow ammos as requested
+     * @param ammoNum number of yellow ammos requested
+     * @return list of yellow ammos requested
+     */
+    public List<Ammo> getNumYellowAmmos(int ammoNum){
+
+        return null;
+    }
+
+    /**
+     * this method gives to player, if possible, red ammos as requested
+     * @param ammoNum number of red ammos requested
+     * @return list of red ammos requested
+     */
+    public List<Ammo> getNumRedAmmos(int ammoNum){
+
+        return null;
+    }
+
+    /**
+     * this method insert in the pool used ammos
+     */
     public void insertAmmos(List<Ammo>){
 
     }
