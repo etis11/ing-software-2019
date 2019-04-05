a579e509be33cb7e98c19873b363d64803231b8c
unknown <alessandro3.passoni@mail.polimi.it>
2019-04-05 21:02:39 +0200

    aggiunte classi Dead, Shoot, ShootPlus

---
+++ src/main/java/Model/Shoot.java
@@ -0,0 +1,37 @@
+package Model;
+
+/**
+ * this class is a type of player state, it is used for shoot action
+ *
+ * @author Alessandro Passoni
+ * @version 1.0
+ */
+public class Shoot extends State{
+
+    /**
+     * this is constructor method
+     */
+    public Shoot(){
+
+    }
+
+    @Override
+    public void tryToRun() {
+
+    }
+
+    @Override
+    public void tryToPickUp() {
+
+    }
+
+    @Override
+    public void tryToShoot() {
+
+    }
+
+    @Override
+    public void tryToUsePowerUp() {
+
+    }
+}
