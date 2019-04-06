688bf7e3324ede22d06cdf1fb7a1c6564da8537c
unknown <alessandro3.passoni@mail.polimi.it>
2019-04-06 10:56:26 +0200

    Aggiunte classi MoreAction, MostAction, Reload

---
+++ src/main/java/Model/MostAction.java
@@ -0,0 +1,19 @@
+package Model;
+
+import jdk.tools.jaotc.binformat.macho.MachOSegment;
+
+/**
+ * this class identify particular action for player based on his damage
+ *
+ * @author Alessandro Passoni
+ * @version 1.0
+ */
+public class MostAction extends Action{
+
+    /**
+     * this is the constructor method
+     */
+    public MostAction(){
+        super();
+    }
+}
