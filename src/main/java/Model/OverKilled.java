1e97a973fd464ae4948c80447029ffa9a3b819b2
unknown <alessandro3.passoni@mail.polimi.it>
2019-04-06 11:15:52 +0200

    Aggiunta classe OverKilled

---
+++ src/main/java/Model/OverKilled.java
@@ -0,0 +1,37 @@
+package Model;
+
+/**
+ * this class is a type of player state, it is used after a player was Overkilled
+ *
+ * @author Alessandro Passoni
+ * @version 1.0
+ */
+public class OverKilled extends State {
+
+    /**
+     * this is the constructor method
+     */
+    public OverKilled(){
+
+    }
+
+    @Override
+    public void tryToRun() {
+
+    }
+
+    @Override
+    public void tryToPickUp() {
+
+    }
+
+    @Override
+    public void tryToShoot() {
+
+    }
+
+    @Override
+    public void tryToUsePowerUp() {
+
+    }
+}
