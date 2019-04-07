f8158ac433f6017371600f14c1db369207fe937f
etis11 <etisspb@gmail.com>
2019-04-07 23:45:41 +0200

    Aggiunte classi PowerUpType (enum) e RegenPoint

---
+++ src/main/java/Model/RegenPoint.java
@@ -0,0 +1,25 @@
+package Model;
+
+import java.util.List;
+
+public class RegenPoint extends Tile {
+
+
+    private List<WeaponCard> weaponCards;
+
+
+    public List<WeaponCard> getWeapons() {
+      return weaponCards;
+    }
+
+    public void setWeaponCards(List<WeaponCard> weaponCards) {
+        this.weaponCards = weaponCards;
+    }
+
+    public void putWeaponCard(WeaponCard weaponCard){
+        this.weaponCards.add(weaponCard);
+    }
+
+
+
+}
