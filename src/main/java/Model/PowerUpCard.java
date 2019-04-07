9ca6b5f0fa2ac7d0d676c6e4c0512e17a4a50150
etis11 <etisspb@gmail.com>
2019-04-07 23:44:33 +0200

    Modifica classe (aggiunta connessione con  enum PowerUpType)

--- src/main/java/Model/PowerUpCard.java
+++ src/main/java/Model/PowerUpCard.java
@@ -5,6 +5,7 @@ import java.awt.*;
 public class PowerUpCard  extends Card{
 
     private Color color;
+    private PowerUpType powerUpType ;
 
     public Color getColor() {
         return this.color;
@@ -14,14 +15,19 @@ public class PowerUpCard  extends Card{
         this.color = color;
     }
 
+    public PowerUpCard(Color color, PowerUpType powerUpType) {
+        this.color = color;
+        this.powerUpType = powerUpType;
+    }
+
     public PowerUpCard(Color color) {
         this.color = color;
     }
     public PowerUpCard() {
     }
 
-    public PowerUpType getPowerUp() {
-        return this.PowerUpType;
+    public PowerUpType getPowerUpType() {
+        return this.powerUpType;
     }
 
 }
