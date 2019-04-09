6606911ec15c5cf59c5cfb43cad66688a9734c8c
Alessandro Passoni <alessandro3.passoni@mail.polimi.it>
2019-04-09 12:25:38 +0200

    Aggiunti test funzionalità classe Loader

--- src/test/java/Test/LoaderTest.java
+++ src/test/java/Test/LoaderTest.java
@@ -55,4 +55,57 @@ public class LoaderTest {
         assertTrue(loader.getNumYellowAmmo() == 1+ ammoToAsk, "ERRORE Munizioni gialle caricate");
 
     }
+
+    @Test
+    public void testAskingReloadOverMaxAmmo(){
+
+        loader = new Loader();
+
+        int ammoToAsk = 3;
+
+        loader.askReload(ammoToAsk, ammoToAsk, ammoToAsk);
+
+        assertTrue(loader.getNumBlueAmmo() == 1+ ammoToAsk, "ERRORE Munizioni blu caricate");
+
+        assertTrue(loader.getNumRedAmmo() == 1+ ammoToAsk, "ERRORE Munizioni rosse caricate");
+
+        assertTrue(loader.getNumYellowAmmo() == 1+ ammoToAsk, "ERRORE Munizioni gialle caricate");
+
+    }
+
+    @Test
+    public void testFull(){
+
+        loader = new Loader();
+
+        int ammoToAsk = 3;
+
+        loader.askReload(ammoToAsk, ammoToAsk, ammoToAsk);
+
+        assertTrue(loader.isFullBlue(), "ERRORE Munizioni blu non piene");
+
+        assertTrue(loader.isFullRed(), "ERRORE Munizioni rosse non piene");
+
+        assertTrue(loader.isFullYellow(), "ERRORE Munizioni gialle non piene");
+
+    }
+
+    @Test
+    public void testPutToAmmo(){
+        loader = new Loader();
+
+        int ammoToAsk = 3;
+
+        loader.askReload(ammoToAsk, ammoToAsk, ammoToAsk);
+        
+        loader.ammoToPool(ammoToAsk, ammoToAsk, ammoToAsk );
+
+        assertTrue(loader.getNumBlueAmmo() == 0, "ERRORE Munizioni blu non svuotate");
+
+        assertTrue(loader.getNumRedAmmo() == 0, "ERRORE Munizioni rosse svuotate");
+
+        assertTrue(loader.getNumYellowAmmo() == 0, "ERRORE Munizioni gialle svuotate");
+    }
+
+
 }
