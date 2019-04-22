f46d1f00af1ea948280b6dad0534b4255389f02d
Oscar Pindaro <oscar.pindaro@gmail.com>
2019-04-22 12:44:09 +0200

    aggiunta dei test che controllano la corretta inserzione dei punti di rigenerazione

--- src/test/java/Test/GameMapTest.java
+++ src/test/java/Test/GameMapTest.java
@@ -2,10 +2,12 @@ package Test;
 
 import Model.GameMap;
 import Model.Room;
+import Model.Tile;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class GameMapTest {
@@ -30,5 +32,49 @@ public class GameMapTest {
 
         assertSame(r, map.getRooms().get(0), ()-> "ERROR: the rooms should be the same");
     }
+
+    @Test
+    void correctRegenPointInsertion(){
+        Tile red = new Tile();
+        Tile blue = new Tile();
+        Tile yellow = new Tile();
+
+        try{
+            map.addRegenPoint("red", red);
+            map.addRegenPoint("blue", blue);
+            map.addRegenPoint("yellow", yellow);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            fail(()-> "ERROR: One of the tile has been inserted in the wrong way");
+        }
+
+        assertSame(red, map.getRedRegenPoint(), ()->"ERROR: The regen points should be the same");
+        assertSame(blue, map.getBlueRegenPoint(), ()->"ERROR: The regen points should be the same");
+        assertSame(yellow, map.getYellowRegenPoint(), ()->"ERROR: The regen points should be the same");
+
+    }
+
+    @Test
+    void correctRegenPointInsertionParametric(){
+        Tile red = new Tile();
+        Tile blue = new Tile();
+        Tile yellow = new Tile();
+
+        try{
+            map.addRegenPoint("red", red);
+            map.addRegenPoint("blue", blue);
+            map.addRegenPoint("yellow", yellow);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            fail(()-> "ERROR: One of the tile has been inserted in the wrong way");
+        }
+
+        assertSame(red, map.getRegenPoint("red"), ()->"ERROR: The regen points should be the same");
+        assertSame(blue, map.getRegenPoint("blue"), ()->"ERROR: The regen points should be the same");
+        assertSame(yellow, map.getRegenPoint("yellow"), ()->"ERROR: The regen points should be the same");
+    }
+
 
 }
