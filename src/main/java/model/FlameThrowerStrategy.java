93d72ed2bca153613152ecade2d0e4b85246b9ef
Oscar Pindaro <oscar.pindaro@gmail.com>
2019-04-28 16:36:39 +0200

    la classe flamethrower è vuota

--- src/main/java/model/FlameThrowerStrategy.java
+++ src/main/java/model/FlameThrowerStrategy.java
@@ -1,4 +1,27 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * the current implementation requires
+ */
 public class FlameThrowerStrategy extends AbstractTargetStrategy {
-}
+
+    public boolean areTargetValid(Player shooter, List<Player> targets) {
+        //checks if throws an exception
+        super.areTargetValid(shooter, targets);
+
+    }
+
+    @Override
+    public boolean canHitSomeone(Player shooter, List<Player> players) {
+
+    }
+
+    }
+
+    @Override
+    public List<Player> hittableTargets(Player shooter, List<Player> players) {
+        return new ArrayList<>();
+    }
