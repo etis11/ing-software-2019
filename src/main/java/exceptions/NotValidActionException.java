fc81fc876d766e92d9b35f53ed2b02b463a909f4
Alessandro Passoni <alessandro3.passoni@mail.polimi.it>
2019-04-30 12:06:13 +0200

    Aggiunta eccezione per gestire le azioni non eseguibili dal player

---
+++ src/main/java/exceptions/NotValidActionException.java
@@ -0,0 +1,24 @@
+package exceptions;
+
+public class NotValidActionException extends Exception{
+    
+    public NotValidActionException() {
+        super();
+    }
+
+    public NotValidActionException(String message) {
+        super(message);
+    }
+
+    public NotValidActionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotValidActionException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotValidActionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
