9606bdc5f6e8742ac5712dd14594cdc47d68ba4c
etis11 <etisspb@gmail.com>
2019-04-30 10:11:10 +0200

    Aggiunti tutti i javadoc e istruzioni mancanti alle due strategie Tractor Beam e Vortex Cannon

--- src/main/java/model/TractorBeamStrategy.java
+++ src/main/java/model/TractorBeamStrategy.java
@@ -3,14 +3,36 @@ package model;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * This class is one of the strategy used to determine the action needed to be done when the effect of the card
+ * is chosen.Basically,what it does is, select a player (let it be visible or not to the shooter) and move the picked
+ * target up to 2 squares keeping in mind that the new position should be a visible tile to the shooter, for then
+ * inflicting the damage
+ * */
 public class TractorBeamStrategy extends AbstractTargetStrategy {
 
+    /**
+     * Private attribute of type GameMap needed to determine other players position based from the shooter's
+     * point of view
+     * */
     private GameMap gameMap;
 
+    /**
+     * This is the constructor of the class
+     * */
     public TractorBeamStrategy (GameMap gameMap){
         this.gameMap = gameMap;
     }
 
+    /**
+     * The following method accepts as input two parameters and is needed to grab all the visible tiles of the player
+     * whose turn is.The way it works is that it checks all the possible tiles of a distance minor to 3 from where the
+     * target is located at and from all of them, returns ONLY the ones that are visible to our shooter
+     * @param shooter needed to check his visible tiles
+     * @param target is the target chosen from another method to be used and grab all the tiles of a distance<3 from
+     *               the target
+     * @return all the tiles are have a distance<3 from the target and at the same time are visible to the shooter
+     * */
     //TODO fare test con metodo distance
     public List<Tile> destinationTiles(Player shooter, Player target){
         List<Tile> visibleTiles = gameMap.allVisibleTiles(shooter);
@@ -23,6 +45,13 @@ public class TractorBeamStrategy extends AbstractTargetStrategy {
         return destinations;
     }
 
+    /**
+     * Classic method used to checker whether or not the list of targets selected by the player (shooter in our case)
+     * are valid or not. Even if just one of them is not possible to be considered as a target, it should return false
+     * @param shooter is used to identify the player whose turn is
+     * @param targets is a list of plausible targets (it doesn't mean they all MUST be true) seen by shooter's perspective
+     * @return a boolean that depends whether or not, the list of targets chosen by the player is totally valid or not
+     * */
     @Override
     public boolean areTargetValid(Player shooter, List<Player> targets) {
         super.areTargetValid(shooter, targets);
@@ -30,6 +59,12 @@ public class TractorBeamStrategy extends AbstractTargetStrategy {
         return visibleTiles.stream().anyMatch(tile ->  tile.distance(targets.get(0))<3 ) ;
     }
 
+    /**
+     * THe following method is used to move the chosen target to the chosen tile by the player
+     * @param shooter is needed to determine who is the shooter(whose turn is moreover)
+     * @param target is the target CHOSEN by the player to be moved
+     * @param destination is the new tile where the target should be moved at
+     * */
     public void moveTarget(Player shooter,  Player target, Tile destination){
         Tile startPoint = target.getTile();
         if(this.destinationTiles(shooter,target).contains(destination)&&hittableTargets(shooter).contains(target)){
--- src/main/java/model/VortexCannonStrategy.java
+++ src/main/java/model/VortexCannonStrategy.java
@@ -8,31 +8,69 @@ import java.util.stream.Collectors;
  * */
 public class VortexCannonStrategy extends AbstractTargetStrategy {
 
+    /**
+     * Private attribute of type GameMap needed to check other players position in the map
+     * */
     private GameMap gameMap;
 
+    /**
+     * List of players that have already been attacked once. The following attribute is used when the optional effect
+     * has been applied also
+     * */
     private List<Player> attackedPlayers;
 
+    /**
+     * This is the constructor of our class
+     * */
      public VortexCannonStrategy (GameMap gameMap){
          this.gameMap = gameMap ;
      }
 
+     /**
+      * Method used to check whether or not the targets chosen by our shooter are all valid or not
+      * @param shooter is the player whose turn is and who got to chose the target/targets
+      * @param targets list of players chosen BY the player to check whether or not they are valid targets or not
+      * @return a boolean that it depends from the list of players selected by the shooter. If all valid then return true
+      * */
     @Override
     public boolean areTargetValid(Player shooter, List<Player> targets) {
         super.areTargetValid(shooter, targets);
         return hittableTargets(shooter).containsAll(targets);
     }
 
+    /**
+     * Method needed to determine whether or not there's at least one target available for the shooter.This way
+     * we can avoid unneeded further methods to be used at
+     * @param shooter is the player whose turn is
+     * @return a boolean that checks whether or not the hittableTargets method is empty or not
+     * */
     @Override
     public boolean canHitSomeone(Player shooter) {
         return !hittableTargets(shooter).isEmpty();
     }
 
+    /**
+     * Method used to return list of all hittable targets from our shooter's map position.
+     * Two conditions are needed to be met so that it gets validated:
+     * 1. Filter all the players that do have a distance minor or equal to 1
+     * 2. Remove yourself from the list that was generated in the first step
+     * @param shooter is needed to check what players can he attack and obviously to remove himself from the list
+     *                of possible targets
+     * @return all the players that can be targeted by the shooter (the shooter isn't included)
+     * */
 //tra tutti i player,filtro quelli che hanno distanza di <=1 da visibletiles e l'altra condizione e' che non sia mestesso
     @Override
     public List<Player> hittableTargets(Player shooter) {
          return Match.getPlayers().stream().filter( player -> gameMap.allVisibleTiles(shooter).stream().filter(tile -> !tile.equals(shooter.getTile())).anyMatch(visible -> (visible.distance(player)<=1)) && !player.equals(shooter) ).collect(Collectors.toList());
     }
 
+    /**
+     * Method used to move the picked target (after verifying that the player is a valid target and destination
+     * is a valid action also.
+     * @param shooter needless to say, is the one who picks the target and target's new destination
+     * @param target is the player who is being targeted by the shooter and whose destination will change
+     * @param destination is the new Tile where the target will get moved into
+     * */
     public void moveTarget(Player shooter,  Player target, Tile destination) {
         Tile startPoint = target.getTile();
         startPoint.getPlayers().remove(target);
@@ -41,19 +79,46 @@ public class VortexCannonStrategy extends AbstractTargetStrategy {
         this.attackedPlayers.add(target);
     }
 
+/**
+ * This method is used in case the player wants to apply the optional effect also, Black Hole. Once the basic effect
+ * has been applied, it checks if the new list of targets is available or not.
+ * @param shooter is the player whose turn is and who gets to choose the target/targets
+ * @param targets is the list of targets picked by the player. This doesn't mean all the targets should be valid
+ * @return true or false based on the list of targets chosen by the player. If all the targets are valid targets,
+ * then return true, else false
+ * */
     public boolean areTargetValidBlackHole(Player shooter, List<Player> targets) {
         super.areTargetValid(shooter, targets);
         return hittableTargetsBlackHole(shooter).containsAll(targets);
     }
 
+    /**
+     * Method used for the optional effect, Black Hole. It checks whether or not the player, after performing
+     * the basic effect, can perform or not the optional one also based on whether or not there is at list one
+     * possible target
+     * @param shooter is the player whose turn is
+     * @return true or false by checking whether or not the hittableTargetsBlackHole is empty or not
+     * */
     public boolean canHitSomeoneBlackHole(Player shooter) {
         return !hittableTargetsBlackHole(shooter).isEmpty();
     }
 
+    /**
+     * Method needed to return the list of players that are valid targets for our player (shooter)
+     * @param shooter is the player whose turn is. It's needed to perform the action later on
+     * @return list of all valid targets seen by our shooter's perspective
+     * */
     public List<Player> hittableTargetsBlackHole(Player shooter) {
         return Match.getPlayers().stream().filter( player -> gameMap.allVisibleTiles(shooter).stream().filter(tile -> !tile.equals(shooter.getTile())).anyMatch(visible -> (visible.distance(player)<=1)) && !player.equals(shooter) && !this.attackedPlayers.contains(player) ).collect(Collectors.toList());
     }
 
+    /**
+     * This method is used to move the hittable targets to their new destination which would be the Tile where the
+     * Black Hole is placed at, chosen by the player
+     * @param shooter  is the player whose turn is.He gets to chose the targets and also where to place the black hole
+     * @param targets is the list of all the VALID players to whom the effect can be applied and chosen by the player
+     * @param destination is the new destination where this/these target/targets will get placed at
+     * */
     public void blackHole (Player shooter,  List<Player> targets, Tile destination){
         hittableTargetsBlackHole(shooter).forEach(player -> moveTarget(shooter,player,destination));
     }
