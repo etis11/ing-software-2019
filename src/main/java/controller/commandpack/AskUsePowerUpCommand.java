149f116d7c467f3dec206708c0ec7b2c8aac0968
Alessandro Passoni <alessandro3.passoni@mail.polimi.it>
2019-05-08 20:54:24 +0200

    Aggiunti comando per chiedere di usare powerup e implementato comando di movimento, sistemata documentazione dei comandi

---
+++ src/main/java/controller/commandpack/AskUsePowerUpCommand.java
@@ -0,0 +1,35 @@
+package controller.commandpack;
+
+import View.AbstractView;
+import model.Match;
+
+import java.util.List;
+
+/**
+ * AskUsePowerUpCommand is an instance of command pattern
+ * to ask if the player are allowed to pick up something in his state of game
+ *
+ * @author Alessandro Passoni
+ * @version 1.0
+ */
+public class AskUsePowerUpCommand extends AbstractCommand {
+
+    public AskUsePowerUpCommand(Match match, AbstractView originView, List<AbstractView> allViews){
+        super(match, originView, allViews);
+    }
+
+    /**
+     * execute the validation of command and notify to views what happen
+     * (only the player who calls if is not allowed, all if he is allowed)
+     */
+    @Override
+    public void execute() {
+        if (!match.getCurrentPlayer().getState().canUsePowerUp()){
+            //TODO originView.onfailure();
+        }
+        else {
+            match.getCurrentPlayer().getState().nextState("PowerUp", match.getCurrentPlayer());
+            //TODO allViews
+        }
+    }
+}
--- src/main/java/controller/commandpack/MoveCommand.java
+++ src/main/java/controller/commandpack/MoveCommand.java
@@ -2,19 +2,46 @@ package controller.commandpack;
 
 import View.AbstractView;
 import model.Match;
+import model.Movement;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * MoveCommand is an instance of command pattern
+ * it allows player to move through tiles
+ *
+ * @author Alessandro Passoni
+ * @version 1.0
+ */
 public class MoveCommand extends AbstractCommand {
 
+    /**
+     * list containing moves to construct a movement
+     */
+    private List<String> moves;
 
-    public MoveCommand(Match match, AbstractView originView, List<AbstractView> allViews){
+    public MoveCommand(Match match, AbstractView originView, List<AbstractView> allViews, List<String> moves){
         super(match, originView, allViews);
-        //TODO mancano parametri per creare movimento e controlli booleani
+        this.moves = moves;
+        //TODO mancano controlli booleani
     }
 
+    /**
+     * execute the validation of command and notify to views what happen
+     *(only the player who calls if is not allowed, all if he is allowed)
+     * in case of positive validation provide to update the remaining moves and moves the player
+     */
     @Override
     public void execute() {
-        //TODO
+        //TODO modificare condizione se ho superato i controlli dei sottostati precedenti
+        if (match.getCurrentPlayer().getState().getRemainingSteps()<moves.size()){
+            //TODO originView.onfailure();
+        }
+        else {
+            match.getCurrentPlayer().getState().decrementReaminingSteps(moves.size());
+            match.getCurrentPlayer().move(new Movement(new ArrayList<String>()));
+            //TODO allViews
+        }
     }
 }
