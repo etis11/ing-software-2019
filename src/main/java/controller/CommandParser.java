c423923b38f23475cee550cbf6b78fc92e498a7e
Alessandro Passoni <alessandro3.passoni@mail.polimi.it>
2019-05-17 23:01:05 +0200

    continuata implementazione CommandParser, ma non mi convincono alcune idee avute

--- src/main/java/controller/CommandParser.java
+++ src/main/java/controller/CommandParser.java
@@ -1,31 +1,57 @@
 package controller;
 
-import controller.commandpack.Command;
+import controller.commandpack.*;
 import exceptions.DuplicateException;
+import model.GameManager;
+import view.MessageListener;
+import view.StringView;
 
+import java.lang.reflect.Constructor;
 import java.util.*;
 
 /**
  * The command parser is a registry in which command can be registered (this operation is usually done during initialization).
  * Then, for usage, the command can be obtained on request. A command is associated to a key (which can be considered his
  * name)
- * @param <T> the type of the key used in the registry.
  */
-public class CommandParser<T>{
+public class CommandParser implements Runnable{
 
     /**
      * registry in which the commands are held
      */
-    private final Map<T, Command> registry = new HashMap<>();
+    private final Map<String, Command> registry = new HashMap<>();
+
+    private boolean quit;
+
+    //TODO sono  qui?
+    private StringView cli;
+
+    private CommandExecutor cmdExe;
+    private GameManager gm;
+    private MessageListener originView;
+    private List<MessageListener> allView;
+
+    public CommandParser(){
+        //TODo implementare
+        this.quit = true;
+        init();
+    }
+
+    private void init(){
+        //TODO non mi convince
+        registerCommand("PickUp", new AskPickCommand(gm, originView, allView));
+        registerCommand("Shoot", new AskShootCommand(gm, originView, allView));
+        registerCommand("Reload", new AskReloadCommand(gm, originView, allView));
+        registerCommand("EndTurn", new AskEndTurnCommand(gm, originView, allView));
+        //TODO come capisco se direzione o nome dell'arma o altro?
+    }
 
     /**
      * Register a command in the parser
      * @param commandName the name of the command
      * @param c the command that has to be registered
-     * @throws DuplicateException if the command is already in the registry or there is already a command with the given key
      */
-
-    public void registerCommand(T commandName, Command c){
+    private void registerCommand(String commandName, Command c){
         if (commandName == null || c == null) throw new IllegalArgumentException("The key or the command are null");
         if(registry.containsKey(commandName)) throw new DuplicateException("There's already a command with this key");
         if(registry.containsValue(c)) throw new DuplicateException("There is already a command with a different key");
@@ -33,33 +59,35 @@ public class CommandParser<T>{
     }
 
     /**
-     * //TODO potrebbe aver senso returnare una copia
      * gets the command with the given command name
      * @param commandName the key
      * @return a command, is never null
      */
-
-    private Command getCommand(T commandName) {
+    private Command getCommand(String commandName) {
         if(commandName == null) throw new  IllegalArgumentException("The key is null");
         if(!registry.containsKey(commandName)) throw new NoSuchElementException("There isnt a command associated to this key");
+       //TODO copia del comando
         return registry.get(commandName);
     }
-
-    /**
-     * Returns a copy of the registry.
-     * //TODO potrebbe essere meglio copiare anche i comandi?
-     * @return
-     */
-
 
     public List<Command> getCommandList() {
         return new ArrayList<>(registry.values());
     }
 
-    public void start(){
-        //TODO while(notquit)  commandName =cli.getString()
-        //command = getCommand(commandName);
-        //copy(command);
-        //executor.addcommand(copia)
+
+    @Override
+    public void run() {
+        String commandName;
+        Command command;
+        while(quit){
+            commandName = cli.getInput();
+            if(!commandName.equalsIgnoreCase("quit")) {
+                command = getCommand(commandName);
+                cmdExe.addCommand(command);
+            }
+            else{
+                quit = false;
+            }
+        }
     }
 }
