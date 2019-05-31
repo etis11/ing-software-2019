56736900e81b8d454b4561c6769bbfb904b2c896
Alessandro Passoni <alessandro3.passoni@mail.polimi.it>
2019-05-31 10:52:05 +0200

    Aggiunte risorse, modificata la gui

--- src/main/java/gui/LobbyFrame.java
+++ src/main/java/gui/LobbyFrame.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
@@ -15,8 +16,27 @@ public class LobbyFrame extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Adrenalina - lobby d'attesa");
+        stage.setTitle("Adrenalina - Lobby d'attesa");
         stage.setResizable(false);
+
+        //path of button image
+        String pathDistruttore = "."+ File.separatorChar+ "src"+ File.separatorChar + "main" + File.separatorChar + "resources"
+                + File.separatorChar +"img"+File.separatorChar+"Distruttore.png";
+        String pathBanshee = "."+ File.separatorChar+ "src"+ File.separatorChar + "main" + File.separatorChar + "resources"
+                + File.separatorChar +"img"+File.separatorChar+"Banshee.png";
+        String pathDozer = "."+ File.separatorChar+ "src"+ File.separatorChar + "main" + File.separatorChar + "resources"
+                + File.separatorChar +"img"+File.separatorChar+"Dozer.png";
+        String pathSprog = "."+ File.separatorChar+ "src"+ File.separatorChar + "main" + File.separatorChar + "resources"
+                + File.separatorChar +"img"+File.separatorChar+"Sprog.png";
+        String pathVioletta = "."+ File.separatorChar+ "src"+ File.separatorChar + "main" + File.separatorChar + "resources"
+                + File.separatorChar +"img"+File.separatorChar+"Violetta.png";
+
+        Image imageDistruttore = new Image(new FileInputStream(pathDistruttore),75,75,false, true);
+        Image imageBanshee = new Image(new FileInputStream(pathBanshee),75,75,false, true);
+        Image imageDozer = new Image(new FileInputStream(pathDozer),75,75,false, true);
+        Image imageSprog = new Image(new FileInputStream(pathSprog),75,75,false, true);
+        Image imageVioletta = new Image(new FileInputStream(pathVioletta),75,75,false, true);
+
 
         Button buttonPlayer1 = new Button();
         Button buttonPlayer2 = new Button();
@@ -28,22 +48,27 @@ public class LobbyFrame extends Application {
         buttonPlayer1.setLayoutX(50);
         buttonPlayer1.setMinWidth(buttonWidth);
         buttonPlayer1.setMinHeight(buttonWidth);
+        buttonPlayer1.setGraphic(new ImageView(imageDistruttore));
         buttonPlayer2.setLayoutY(buttonPlayer1.getLayoutY());
         buttonPlayer2.setLayoutX(250);
         buttonPlayer2.setMinWidth(buttonWidth);
         buttonPlayer2.setMinHeight(buttonWidth);
+        buttonPlayer2.setGraphic(new ImageView(imageBanshee));
         buttonPlayer3.setLayoutY(buttonPlayer1.getLayoutY());
         buttonPlayer3.setLayoutX(450);
         buttonPlayer3.setMinWidth(buttonWidth);
         buttonPlayer3.setMinHeight(buttonWidth);
+        buttonPlayer3.setGraphic(new ImageView(imageDozer));
         buttonPlayer4.setLayoutY(buttonPlayer1.getLayoutY());
         buttonPlayer4.setLayoutX(650);
         buttonPlayer4.setMinWidth(buttonWidth);
         buttonPlayer4.setMinHeight(buttonWidth);
+        buttonPlayer4.setGraphic(new ImageView(imageSprog));
         buttonPlayer5.setLayoutY(buttonPlayer1.getLayoutY());
         buttonPlayer5.setLayoutX(850);
         buttonPlayer5.setMinWidth(buttonWidth);
         buttonPlayer5.setMinHeight(buttonWidth);
+        buttonPlayer5.setGraphic(new ImageView(imageVioletta));
 
         //TODO rendere statici queste variabili?
         //path of background image
