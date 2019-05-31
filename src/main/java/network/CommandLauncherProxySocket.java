cbaecb9b96ac42abc04695af66f9ac0ad35cb3f7
Oscar Pindaro <oscar.pindaro@gmail.com>
2019-05-31 16:30:28 +0200

    aggiunte di piccole modifiche

--- src/main/java/network/CommandLauncherProxySocket.java
+++ src/main/java/network/CommandLauncherProxySocket.java
@@ -1,6 +1,5 @@
 package network;
 
-import controller.CommandExecutor;
 import controller.commandpack.Command;
 import controller.commandpack.CommandContainer;
 
@@ -10,9 +9,9 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 /**
- * This class behave like a command Container, but insted of adding a command in a pool, sends it to the executor on the server
+ * This class behave like a command Container, but instead of adding a command in a pool, sends it to the executor on the server
  */
-public class CommandExecutorProxySocket implements CommandContainer {
+public class CommandLauncherProxySocket implements CommandContainer {
 
     Socket socket;
     ObjectInputStream in;
@@ -24,7 +23,7 @@ public class CommandExecutorProxySocket implements CommandContainer {
      * @param port port on which is listening
      * @throws IOException problems while getting the input and output stream
      */
-    public CommandExecutorProxySocket(String host, int port) throws IOException {
+    public CommandLauncherProxySocket(String host, int port) throws IOException {
         socket = new Socket(host, port);
         in = new ObjectInputStream(socket.getInputStream());
         out = new ObjectOutputStream(socket.getOutputStream());
@@ -46,7 +45,7 @@ public class CommandExecutorProxySocket implements CommandContainer {
     }
 
     /**
-     * close the socket. After this method, the copmmand executor is not able to send commands on the net anymore
+     * close the socket. After this method, the command executor is not able to send commands on the net anymore
      */
     public void close(){
         try {
