d697aed7f5938fab256f4653029e588cdef10384
Alessandro Passoni <alessandro3.passoni@mail.polimi.it>
2019-06-08 00:29:40 +0200

    modificati alcuni identificatori

--- src/main/java/view/PlayerObserver.java
+++ src/main/java/view/PlayerObserver.java
@@ -3,8 +3,8 @@ package view;
 import model.Player;
 
 public interface PlayerObserver {
-    public void onHpChange(Player damagePlayer);
-    public void onMarksChange(Player markedPlayer);
-    public void onAmmoChange(Player p);
-    public void onPowerUpChange(Player p);
+     void onHpChange(Player damagePlayer);
+     void onMarksChange(Player markedPlayer);
+     void onAmmoChange(Player p);
+     void onPowerUpChange(Player p);
 }
