88f64b3b88231f1ee6955d491620c0f204f62cfb
Oscar Pindaro <oscar.pindaro@gmail.com>
2019-06-07 11:04:53 +0200

    cambiate le scadenze, aggiunto semplified player

--- src/main/java/model/clientModel/SemplifiedMap.java
+++ src/main/java/model/clientModel/SemplifiedMap.java
@@ -1,4 +1,8 @@
 package model.clientModel;
 
+import model.Tile;
+
 public class SemplifiedMap {
+
+    private Tile[][] map;
 }
---
+++ src/main/java/model/clientModel/SemplifiedPlayer.java
@@ -0,0 +1,8 @@
+package model.clientModel;
+
+public class SemplifiedPlayer {
+    @Override
+    public String toString() {
+        return "stringa del player";
+    }
+}
