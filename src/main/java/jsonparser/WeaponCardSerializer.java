376e05596c9b00f1fa6185aceca8f1253b82c34a
Oscar Pindaro <oscar.pindaro@gmail.com>
2019-06-10 17:09:09 +0200

    ora il weapon serializer è in grado di serializzare sia armi per player, sia armi per tiles. Bisogna settare un po' di boolean

--- src/main/java/jsonparser/WeaponCardSerializer.java
+++ src/main/java/jsonparser/WeaponCardSerializer.java
@@ -15,14 +15,29 @@ public class WeaponCardSerializer implements JsonSerializer<WeaponCard> {
 
     private final Match match;
     private Player currentPlayer;
+    private boolean playerMode;
+    private boolean tileMode;
 
     public WeaponCardSerializer(Match m){
         this.match = m;
+        playerMode = true;
+        tileMode = false;
     }
 
     public void setCurrentPlayer(Player p){
         currentPlayer = p;
     }
+
+    public void setPlayerModeTrue(){
+        playerMode = true;
+        tileMode = false;
+    }
+
+    public void setTileModeTrue(){
+        playerMode = false;
+        tileMode = true;
+    }
+
 
     @Override
     /**
@@ -31,20 +46,31 @@ public class WeaponCardSerializer implements JsonSerializer<WeaponCard> {
      */
     public JsonElement serialize(WeaponCard weaponCard, Type type, JsonSerializationContext jsonSerializationContext) {
         final JsonObject jsonWeaponCard = new JsonObject();
-        if (currentPlayer.getWeapons().contains(weaponCard) || !weaponCard.isLoaded()){
-            jsonWeaponCard.addProperty("name", weaponCard.getName());
-
-            final JsonElement weaponCost= jsonSerializationContext.serialize(weaponCard.getReloadCost(), String[].class);
-            jsonWeaponCard.add("cost", weaponCost);
-
-            jsonWeaponCard.addProperty("loaded", weaponCard.isLoaded());
-
-            final JsonElement baseEffects= jsonSerializationContext.serialize(weaponCard.getBaseEffect(), Effect[].class);
-            jsonWeaponCard.add("baseEffect", baseEffects);
-
-            final JsonElement advancedEffects= jsonSerializationContext.serialize(weaponCard.getAdvancedEffect(), Effect[].class);
-            jsonWeaponCard.add("baseEffect", advancedEffects);
+        if (playerMode){
+            if (currentPlayer.getWeapons().contains(weaponCard) || !weaponCard.isLoaded()){
+                serializeWeaponCard(weaponCard, jsonWeaponCard, jsonSerializationContext);
+            }
         }
+        else{
+            serializeWeaponCard(weaponCard, jsonWeaponCard, jsonSerializationContext);
+        }
+
         return jsonWeaponCard;
+
+    }
+
+    public void serializeWeaponCard(WeaponCard weaponCard, JsonObject jsonWeaponCard, JsonSerializationContext jsonSerializationContext){
+        jsonWeaponCard.addProperty("name", weaponCard.getName());
+
+        final JsonElement weaponCost= jsonSerializationContext.serialize(weaponCard.getReloadCost(), String[].class);
+        jsonWeaponCard.add("cost", weaponCost);
+
+        jsonWeaponCard.addProperty("loaded", weaponCard.isLoaded());
+
+        final JsonElement baseEffects= jsonSerializationContext.serialize(weaponCard.getBaseEffect(), Effect[].class);
+        jsonWeaponCard.add("baseEffect", baseEffects);
+
+        final JsonElement advancedEffects= jsonSerializationContext.serialize(weaponCard.getAdvancedEffect(), Effect[].class);
+        jsonWeaponCard.add("baseEffect", advancedEffects);
     }
 }
