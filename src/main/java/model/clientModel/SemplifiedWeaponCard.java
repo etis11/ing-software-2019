3aa26c169a7d5e6651caa4cef11e0316f4b30b4d
Oscar Pindaro <oscar.pindaro@gmail.com>
2019-06-14 11:26:34 +0200

    aggiunti getters e setter per le weapon cards

--- src/main/java/model/clientModel/SemplifiedWeaponCard.java
+++ src/main/java/model/clientModel/SemplifiedWeaponCard.java
@@ -9,4 +9,44 @@ public class SemplifiedWeaponCard {
     private boolean loaded;
     private List<SemplifiedEffect> baseEffect;
     private List<SemplifiedEffect> advancedEffect;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getCost() {
+        return cost;
+    }
+
+    public void setCost(List<String> cost) {
+        this.cost = cost;
+    }
+
+    public boolean isLoaded() {
+        return loaded;
+    }
+
+    public void setLoaded(boolean loaded) {
+        this.loaded = loaded;
+    }
+
+    public List<SemplifiedEffect> getBaseEffect() {
+        return baseEffect;
+    }
+
+    public void setBaseEffect(List<SemplifiedEffect> baseEffect) {
+        this.baseEffect = baseEffect;
+    }
+
+    public List<SemplifiedEffect> getAdvancedEffect() {
+        return advancedEffect;
+    }
+
+    public void setAdvancedEffect(List<SemplifiedEffect> advancedEffect) {
+        this.advancedEffect = advancedEffect;
+    }
 }
