4f0fb7160dd255582fc1422feb34bb9ef2a05cd1
Oscar Pindaro <oscar.pindaro@gmail.com>
2019-06-20 13:33:41 +0200

    risolto i conflitti che avevamo con il json creator

--- src/test/java/Test/JsonCreatorTest.java
+++ src/test/java/Test/JsonCreatorTest.java
@@ -33,8 +33,8 @@ public class JsonCreatorTest {
         map = new GameMap();
         map.addRegenPoint("red", new Tile(true, false));
         m.setMap(map);
-        GameManager gm = new GameManager(m);
-        jsonCreator = new JsonCreator(gm);
+        GameManager gm = new GameManager();
+        jsonCreator = new JsonCreator();
     }
 
     @Test
