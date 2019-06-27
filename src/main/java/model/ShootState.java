6616c263a667384af714ac6f4dbfac824d6cc957
Alessandro Passoni <alessandro3.passoni@mail.polimi.it>
2019-06-27 10:49:59 +0200

    creati stati per lo sparo

---
+++ src/main/java/model/ShootState.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.Map;
+
+public enum ShootState {
+
+    BASE(null),
+    ASKEDSHOOT(null),
+    CHOSENWEAPON(null),
+    CHOSENEFFECT(null),
+    MOVEEFFECTBASE(null),
+    MOVEEFFECTOPTIONAL(null),
+    TARGETASKED(null),
+    APPLYEFFECTDAMAGE(null),
+    MOVESECONDLY(null),
+    ENDSHOOT(null);
+
+    private Map<String, ShootState> nextPossibleState;
+
+    private ShootState(Map<String, ShootState> nextPossibleState){
+        this.nextPossibleState = nextPossibleState;
+    }
+
+    public Map<String, ShootState> getNextPossibleState(){
+        return nextPossibleState;
+    }
+}
